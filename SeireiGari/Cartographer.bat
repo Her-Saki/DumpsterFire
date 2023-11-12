814b5901a8fa19e21fe9e9ef1e6700fbfdef6584
Her-Saki <63907502+Her-Saki@users.noreply.github.com>
2023-11-12 14:58:49 -0300

    Add files via upload

---
+++ SeireiGari/Cartographer.bat
@@ -0,0 +1,2 @@
+cartographer Seirei_Gari_(Japan).nes Seirei_Gari_commands.txt Japanese_Script -s > Cartographer.log
+pause
---
+++ SeireiGari/Compile.bat
@@ -0,0 +1,3 @@
+copy Seirei_Gari_(Japan).nes Seirei_Gari_(Test).nes
+schasm Code.asm -o Seirei_Gari_(Test).nes -m nes -wrnerr
+@pause
